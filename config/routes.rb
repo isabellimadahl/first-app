cfc8956cbe407b9260e367926a21c5aefe39a793
isabellimadahl <isabel.li.dahl@gmail.com>
2015-06-05 11:52:31 -0400

    seriously

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    first-app::Application.routes.draw do
+    FirstApp::Application.routes.draw do
         root "application#hello"
         
   # The priority is based upon order of creation: first created -> highest priority.
